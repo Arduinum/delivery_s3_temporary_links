136ca2ff922aee6cd8bd931c340892d36d1822bb
Arduinum628 <message.chaos628@gmail.com>
2026-03-06 20:27:20 +0300

    devops: added allowed_hosts and headers

--- src/delivery_s3_temporary_links/main.py
+++ src/delivery_s3_temporary_links/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 
 from delivery_s3_temporary_links.routes.links_s3 import file_s3_router
 from delivery_s3_temporary_links.routes.auth import auth_router
+from delivery_s3_temporary_links.core.settings import settings
+
 
 app = FastAPI()
 
+# Подключение роутеров
 app.include_router(file_s3_router)
 app.include_router(auth_router)
+
+# Подключение middlewares
+app.add_middleware(
+    TrustedHostMiddleware,  # type: ignore
+    allowed_hosts=settings.settings_app.get_domains
+)
